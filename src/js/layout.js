ee98a681043e64b8d4a4a3e9e0f8c0818d87e88f
ramses <cevallosramses@gmail.com>
2023-07-28 14:43:27 -0600

    added gh-pages

--- src/js/layout.js
+++ src/js/layout.js
@@ -15,13 +15,11 @@ import Footer from "./component/Footer";
 
 //create your first component
 const Layout = () => {
-	//the basename is used when your project is published in a subdirectory and not in the root of the domain
-	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
-	const basename = process.env.BASENAME || "";
+	
 
 	return (
 		<div>
-			<BrowserRouter basename={basename}>
+			<BrowserRouter >
 				<ScrollToTop>
 					<Navbar />
 					<Switch>
